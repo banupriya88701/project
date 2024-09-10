500d1a94fca007a7e6d2637d7f765b44d137015b
banpriya88701 <banupriya88701@gmail.com>
2024-09-10 13:26:55 +0530

    project

---
+++ src/gitt/org/GitBasicPro.java
@@ -0,0 +1,26 @@
+package gitt.org;
+
+public class GitBasicPro {
+	
+	
+	private void name() {
+		System.out.println("Banu");
+	}
+	
+	private void age() {
+		System.out.println("27");
+	}
+	
+	private void place() {
+		System.out.println("Chennai");
+	}
+	
+	public static void main(String[] args) {
+		
+		GitBasicPro g=new GitBasicPro();
+		
+		
+		
+	}
+
+}
